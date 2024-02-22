1c9f6f7a00cad0755569b8d3e3a48bd23632f694
cabreraharry <harryericsoncabrera15@gmail.com>
2024-02-22 18:41:22 +0800

    Initial front end commit, slides problem still unsolved

---
+++ main.js
@@ -0,0 +1,10 @@
+// Swiper JS
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
