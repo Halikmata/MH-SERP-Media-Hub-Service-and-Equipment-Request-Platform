cf1f73e6456a423cf88a63e3d9d753f1c1a2700c
elsidRickLopezPanolino <elsidpanolino@gmail.com>
2024-02-28 22:27:06 +0800

    initial header style based on prototype

--- frontend/src/includes/header.jsx
+++ frontend/src/includes/header.jsx
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header style={headerStyle}>
-      <h2>LOGO</h2>
-      <h2>PSU Media Hub</h2>
       <nav>
         <ul style={navStyle}>
-        <li><Link style={liStyle} to="/">Home</Link></li>
+          <li><Link style={liStyle} to="/">Home</Link></li>
           <li><Link style={liStyle} to="/request">Request</Link></li>
           <li><Link style={liStyle} to="/equipment">Equipment</Link></li>
           <li><Link style={liStyle} to="/services">Services</Link></li>
-          <li><Link style={liStyle} to="/about">About US</Link></li>
+          <li><Link style={liStyle} to="/about">About Us</Link></li>
         </ul>
       </nav>
     </header>
@@ -20,22 +18,26 @@ function Header() {
 }
 
 const headerStyle = {
-  background: '#333',
-  color: '#fff',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width:'100vw',
+  minHeight: '60px',
   textAlign: 'center',
-  padding: '10px'
+  padding: '0',
+  boxShadow: '0 3px 5px 5px #88888888'
 }
 
 const navStyle = {
   listStyle: 'none',
   display: 'flex',
   justifyContent: 'center',
-  color: '#fff'
 }
 
 const liStyle = {
-    color: '#fff',
-    margin: '5px 10px',
+  textDecoration: 'none',
+  margin: '5px 10px',
+  color: '#000000',
 }
 
 export default Header;
