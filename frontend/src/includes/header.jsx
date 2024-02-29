299c2b84e6776f30f46d410bdd78d3af3edfbd50
elsidRickLopezPanolino <elsidpanolino@gmail.com>
2024-02-29 18:41:55 +0800

    style navbar

--- frontend/src/includes/header.jsx
+++ frontend/src/includes/header.jsx
@@ -1,43 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
 
 function Header() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header style={headerStyle}>
-      <nav>
-        <ul style={navStyle}>
-          <li><Link style={liStyle} to="/">Home</Link></li>
-          <li><Link style={liStyle} to="/request">Request</Link></li>
-          <li><Link style={liStyle} to="/equipment">Equipment</Link></li>
-          <li><Link style={liStyle} to="/services">Services</Link></li>
-          <li><Link style={liStyle} to="/about">About Us</Link></li>
-        </ul>
-      </nav>
+      <div style={{ display: 'flex', margin: '0 30px' }}>
+        <img src={logo} style={{ width: '50px' }} alt="Logo" />
+      </div>
+      {isSmallScreen ? (
+        <div>
+          <button style={linkStyle}>Menu</button>
+        </div>
+      ) : (
+        <>
+        <nav>
+          <ul style={navStyle}>
+            <li><Link style={liStyle} to="/">Home</Link></li>
+            <li><Link style={liStyle} to="/request">Request</Link></li>
+            <li><Link style={liStyle} to="/equipment">Equipment</Link></li>
+            <li><Link style={liStyle} to="/services">Services</Link></li>
+            <li><Link style={liStyle} to="/about">About Us</Link></li>
+          </ul>
+        </nav>
+        <div>
+          <a style={linkStyle}>Log in</a>
+          <a style={linkStyle}>Sign Up</a>
+        </div>
+        </>
+      )}
     </header>
   );
 }
 
 const headerStyle = {
   display: 'flex',
-  justifyContent: 'center',
+  justifyContent: 'space-between',
   alignItems: 'center',
-  width:'100vw',
+  width: '100vw',
   minHeight: '60px',
   textAlign: 'center',
   padding: '0',
   boxShadow: '0 3px 5px 5px #88888888'
-}
+};
 
 const navStyle = {
   listStyle: 'none',
   display: 'flex',
-  justifyContent: 'center',
-}
+  flex: '1',
+  justifyContent: 'space-between',
+};
 
 const liStyle = {
   textDecoration: 'none',
   margin: '5px 10px',
   color: '#000000',
-}
+};
+
+const linkStyle = {
+  textDecoration: 'none',
+  margin: '0 10px',
+  color: '#000000',
+};
 
 export default Header;
