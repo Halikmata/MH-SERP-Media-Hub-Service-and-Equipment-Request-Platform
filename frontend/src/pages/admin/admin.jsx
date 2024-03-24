271d8e8da915a80d239920d4c149bac180516719
elsidRickLopezPanolino <elsidpanolino@gmail.com>
2024-03-24 22:04:18 +0800

    admin table

--- frontend/src/pages/admin/admin.jsx
+++ frontend/src/pages/admin/admin.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import AdminTable from './component/AdminTable';
+
 const Admin = () => {
 
   return (
     <div>
       <h1>Admin</h1>
+      <AdminTable collection={"equipment"} not_include={["description"]}></AdminTable>
+      <AdminTable collection={"services"} not_include={["idservice"]}></AdminTable>
+      <AdminTable collection={"requests"}></AdminTable>
     </div>
   );
 };
