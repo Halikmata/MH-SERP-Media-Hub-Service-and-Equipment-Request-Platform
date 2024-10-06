345fb5fa9d5e5b1858308215216ae42b7c9d868f
KenLorenz <kenlorenz420@gmail.com>
2024-10-06 15:57:50 +0800

    removed unused files

--- backend/decryption.py
+++
@@ -1,9 +0,0 @@
-# this python file is used to decrypt credentials that were encrypted by bcrypt.
-# 
-# input: encrypt/decrypt --> collection name --> multiple/single --> objectid (the string within the quotes of ObjectID), this phase is optional if multiple
-#                        --> attribute of the chosen collection
-# 
-# output: encrypted/decrypted attribute instance/s
-
-while True:
-    exit()
