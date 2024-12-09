ef376dc7fc8124585b94e44d61c6f335a209950c
KenLorenz <kenlorenz420@gmail.com>
2024-12-09 09:48:20 +0800

    removed deprecated css

--- frontend/src/pages/admin/component/CalendarRequests.jsx
+++ frontend/src/pages/admin/component/CalendarRequests.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './CalendarRequests.css'
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = ({ requestData }) => {
